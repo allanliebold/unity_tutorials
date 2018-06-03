7195c1f9830358bf474413e1ea0e5931bb0e5d2c
Allan Liebold <allan.liebold@gmail.com>
2018-06-02 20:59:11 -0700

    Update PlayerController.cs

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -3,4 +3,9 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+  void FixedUpdate () 
+  {
+     float moveHorizontal = Input.GetAxis ("Horizontal");
+     float moveVertical = Input.GetAxis ("Vertical");
+  }
 }
