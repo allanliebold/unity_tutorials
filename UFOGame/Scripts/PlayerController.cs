eb328165187e022901c723696a241fb128d75bfb
Allan Liebold <allan.liebold@gmail.com>
2018-06-04 06:02:06 -0700

    Update PlayerController.cs

--- UFOGame/Scripts/PlayerController.cs
+++ UFOGame/Scripts/PlayerController.cs
@@ -20,6 +20,9 @@ public class PlayerController : MonoBehaviour {
   
   void OnTriggerEnter2D(Collider2D other)
   {
-    
+    if (other.gameObject.CompareTag("PickUp"))
+    {
+      other.gameObject.SetActive(false);  
+    }
   }
 }
