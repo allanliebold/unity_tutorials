3cb6cb0ff1a4c94ba9b7779fe2911f3b84f0207f
Allan Liebold <allan.liebold@gmail.com>
2018-06-05 05:55:58 -0700

    Update Board.cs

--- Match3/Scripts/Board.cs
+++ Match3/Scripts/Board.cs
@@ -12,12 +12,16 @@ public class Board : MonoBehaviour {
   
   void Start () {
     m_allTiles = new Tile[width,height];
+    SetupTiles();
   }
   
   void SetupTiles() {
      for (int i = 0; i < width; i++) {
       for(int j = 0; i < height; j++) {
-        GameObject tile = Instantiate (tilePrefab, new Vector3(i, j, 0), Quaternion.identity) as GameObject; 
+        GameObject tile = Instantiate (tilePrefab, new Vector3(i, j, 0), Quaternion.identity) as GameObject;
+        tile.name = "Tile (" + i + "," + j + ")";
+        m_allTiles [i, j] = tile.GetComponent<Tile>();
+        tile.transform.parent = transform;
       }       
      }
   }
