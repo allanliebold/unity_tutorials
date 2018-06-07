94d9b3008f5672f6e24042afe24c7698cf59fa2f
Allan Liebold <allan.liebold@gmail.com>
2018-06-07 09:44:08 -0700

    Update Tile.cs

--- Match3/Scripts/Tile.cs
+++ Match3/Scripts/Tile.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public int xIndex;
 public int yIndex;
 
+Board m_board;
+
 public class Tile : MonoBehaviour {
   void Init(int x, int y, Board board) {
     xIndex = x;
     yIndex = y;
+    m_board = board;
   }
 }
