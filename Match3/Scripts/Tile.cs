a05c00216e620d4af61a16f0663734e19aab8c3c
Allan Liebold <allan.liebold@gmail.com>
2018-06-07 09:45:10 -0700

    Update Tile.cs

--- Match3/Scripts/Tile.cs
+++ Match3/Scripts/Tile.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
 using System.Collections; 
 
-public int xIndex;
-public int yIndex;
-
-Board m_board;
-
 public class Tile : MonoBehaviour {
-  void Init(int x, int y, Board board) {
+
+  public int xIndex;
+  public int yIndex;
+
+  Board m_board;
+
+  public void Init(int x, int y, Board board) {
     xIndex = x;
     yIndex = y;
     m_board = board;
