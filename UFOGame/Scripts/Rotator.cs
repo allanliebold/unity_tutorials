0b5d13800fd2465129cac1f742d2e3dccbb9d446
Allan Liebold <allan.liebold@gmail.com>
2018-06-11 17:14:46 -0700

    Update Rotator.cs

--- UFOGame/Scripts/Rotator.cs
+++ UFOGame/Scripts/Rotator.cs
@@ -2,8 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 public class CompleteRotator : MonoBehaviour {
-  void Update ()
-  {
+  void Update () {
     transform.Rotate (new Vector3 (0, 0, 45) * Time.deltaTime);  
   }
 }
