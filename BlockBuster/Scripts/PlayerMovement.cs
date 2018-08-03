bb7f24964badf6ab09875bcb2f35f5be4cae3c61
Allan Liebold <allan.liebold@gmail.com>
2018-08-03 05:58:21 -0700

    Update PlayerMovement.cs

--- BlockBuster/Scripts/PlayerMovement.cs
+++ BlockBuster/Scripts/PlayerMovement.cs
@@ -6,4 +6,15 @@ public class PlayerMovement : MonoBehaviour {
   public float acceleration;
   public float maxSpeed; 
   
+  private Rigidbody rigidBody;
+  private KeyCode[] inputKeys;
+  private Vector3[] directionsForKeys;
+  
+  void Start () {
+    
+  }
+  
+  void FixedUpdate () {
+    
+  }
 }
