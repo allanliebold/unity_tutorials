cfb17e201774a837d452680058ef1fc365e5f8ed
Allan Liebold <allan.liebold@gmail.com>
2018-08-03 05:59:49 -0700

    Update PlayerMovement.cs

--- BlockBuster/Scripts/PlayerMovement.cs
+++ BlockBuster/Scripts/PlayerMovement.cs
@@ -13,6 +13,7 @@ public class PlayerMovement : MonoBehaviour {
   void Start () {
     inputKeys = new KeyCode[] { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D };
     directionsForKeys = new Vector3[] { Vector3.forward, Vector3.left, Vector3.back, Vector3.right };
+    rigidBody = GetComponent<Rgidibody> ();
   }
   
   void FixedUpdate () {
