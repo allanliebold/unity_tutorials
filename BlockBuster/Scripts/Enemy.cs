48b15704eeb8a2c1adf152930925f55baecc115b
Allan Liebold <allan.liebold@gmail.com>
2018-08-04 21:21:33 -0700

    Update Enemy.cs

--- BlockBuster/Scripts/Enemy.cs
+++ BlockBuster/Scripts/Enemy.cs
@@ -1,1 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Enemy : MonoBehaviour {
+  public float moveSpeed;
+  public int health;
+  public int damage;
+  public Transform targetTransform;
+}
