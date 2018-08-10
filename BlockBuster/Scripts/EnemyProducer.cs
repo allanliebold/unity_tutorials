13a0a4616a06d4370161ca75b4101fdde1d88c84
Allan Liebold <allan.liebold@gmail.com>
2018-08-10 06:10:51 -0700

    Update EnemyProducer.cs

--- BlockBuster/Scripts/EnemyProducer.cs
+++ BlockBuster/Scripts/EnemyProducer.cs
@@ -11,6 +11,12 @@ public class EnemyProducer : MonoBehaviour {
   private Bounds spawnArea;
   private GameObject player;
   
+  void Start() {
+    spawnArea = this.GetComponent<BoxCollider>().bounds;  
+    SpawnEnemies(shouldSpawn);
+    InvokeRepeating("spawnEnemy", 0.5f, 1.0f);
+  }
+  
   public void SpawnEnemies(bool shouldSpawn) {
     if(shouldSpawn) {
       player = GameObject.FindGameObjectWithTag("Player");
@@ -18,9 +24,5 @@ public class EnemyProducer : MonoBehaviour {
     this.shouldSpawn = shouldSpawn;
   }
   
-  void Start() {
-    spawnArea = this.GetComponent<BoxCollider>().bounds;  
-    SpawnEnemies(shouldSpawn);
-    InvokeRepeating("spawnEnemy", 0.5f, 1.0f);
-  }
+
 }
