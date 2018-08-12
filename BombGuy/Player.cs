4bed52d75140d3f64d9a97b2f07dc7f0ba390bbe
Allan Liebold <allan.liebold@gmail.com>
2018-08-11 22:32:52 -0700

    Update Player.cs

--- BombGuy/Player.cs
+++ BombGuy/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour {
 
   private void DropBomb() {
     if (bombPrefab) {
-      
+      Instantiate(bombPrefab, myTransform.position, bombPrefab.transform.rotation);  
     }
   }
 }
