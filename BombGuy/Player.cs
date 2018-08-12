2dbe1f5599f1bedb19a06c13ae13f55c79f64b66
Allan Liebold <allan.liebold@gmail.com>
2018-08-11 22:34:30 -0700

    Update Player.cs

--- BombGuy/Player.cs
+++ BombGuy/Player.cs
@@ -6,7 +6,9 @@ public class Player : MonoBehaviour {
 
   private void DropBomb() {
     if (bombPrefab) {
-      Instantiate(bombPrefab, new Vector3(Mathf.RoundToInt(myTransform.position.x), bombPrefab.transform.position.y, Mathf.RoundToInt(myTransform.position.z)), bombPrefab.transform.rotation);
+      Instantiate(bombPrefab, new Vector3(Mathf.RoundToInt(myTransform.position.x), 
+                  bombPrefab.transform.position.y, Mathf.RoundToInt(myTransform.position.z)), 
+                  bombPrefab.transform.rotation);
     }
   }
 }
