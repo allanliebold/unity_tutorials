1cded05b2cbef0bd8bd0d054d3e936d006b159eb
Allan Liebold <allan.liebold@gmail.com>
2018-08-13 00:35:43 -0700

    Update Bomb.cs

--- BombGuy/Bomb.cs
+++ BombGuy/Bomb.cs
@@ -29,6 +29,7 @@ public class Bomb : MonoBehaviour {
   private IEnumerator CreateExplosions(Vector3 direction) {
     for (int i = 1; i < 3; i++) {
       RaycastHit hit;
+      Physics.Raycast(transform.position + new Vector3(0, .5f, 0), direction, out hit, i, levelMask);
     }
   }
 }
