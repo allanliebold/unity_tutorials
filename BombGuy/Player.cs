30fb13789cdbd37f500770222b013ff3f3ac36c3
Allan Liebold <allan.liebold@gmail.com>
2018-08-14 04:39:20 -0700

    Update Player.cs

--- BombGuy/Player.cs
+++ BombGuy/Player.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-
+  [Range (1, 2)]
+  public int playerNumber = 1; 
+  
   private void DropBomb() {
     if (bombPrefab) {
       Instantiate(bombPrefab, new Vector3(Mathf.RoundToInt(myTransform.position.x), 
