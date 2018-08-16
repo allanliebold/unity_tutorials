275da688a58f1adb39b6f74b77215fcd941022d4
Allan Liebold <allan.liebold@gmail.com>
2018-08-16 00:08:34 -0700

    Update Bomb.cs

--- BombGuy/Bomb.cs
+++ BombGuy/Bomb.cs
@@ -44,7 +44,7 @@ public class Bomb : MonoBehaviour {
   
   public void OnTriggerEnter(Collider other) {
     if (!exploded && other.CompareTag("Explosion")) {
-      
+      CancelInvoke("Explode");
     }
   }
 }
