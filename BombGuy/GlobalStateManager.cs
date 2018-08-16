511f395926bc7fb6517f1251ed8313132420d522
Allan Liebold <allan.liebold@gmail.com>
2018-08-16 00:14:23 -0700

    Update GlobalStateManager.cs

--- BombGuy/GlobalStateManager.cs
+++ BombGuy/GlobalStateManager.cs
@@ -4,5 +4,6 @@ using System.Collections;
 public class GlobalStateManager : MonoBehaviour {
   public void PlayerDied(int playerNumber) {
     private int deadPlayers = 0;
+    private int deadPlayerNumber = -1;
   }
 }
