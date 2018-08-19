c71682f5f255a7c7e39c07e5bf4c13605343ec2f
Allan Liebold <allan.liebold@gmail.com>
2018-08-19 11:10:23 -0700

    Update PowerUp.cs

--- PowerUps/PowerUp.cs
+++ PowerUps/PowerUp.cs
@@ -22,6 +22,6 @@ public class PowerUp : MonoBehaviour {
   protected PowerUpState powerUpState;
   
   protected virtual void Awake() {
-    
+    spriteRenderer = GetComponent<SpriteRenderer>();
   }
 }
