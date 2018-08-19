542f6fb00efebf5c2c3521a13399c650e8c29239
Allan Liebold <allan.liebold@gmail.com>
2018-08-19 11:08:23 -0700

    Update PowerUp.cs

--- PowerUps/PowerUp.cs
+++ PowerUps/PowerUp.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class PowerUp : MonoBehaviour {
+  public string powerUpName;
+  public string powerUpExplanation;
+  public string powerUpQuote;
   
+  public bool expiresImmediately;
+  public GameObject specialEffect;
+  public AudioClip soundEffect;
 }
