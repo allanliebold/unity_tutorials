b8af5dc72e2e24b65bdfec6b7b7aad82952ba75f
Allan Liebold <allan.liebold@gmail.com>
2018-08-25 10:48:52 -0700

    Update BoardManager.cs

--- Match3/Scripts/Tutorial/BoardManager.cs
+++ Match3/Scripts/Tutorial/BoardManager.cs
@@ -30,7 +30,7 @@ public class BoardManager : MonoBehaviour {
 				GameObject newTile = Instantiate(tile, new Vector3(startX + (xOffset * x), startY + (yOffset * y), 0), tile.transform.rotation);
 				tiles[x, y] = newTile;
 				newTile.transform.parent = transform;
-				Sprite newSprite;
+				Sprite newSprite = characters[Random.Range(0, characters.Count)];
 			}
         }
     }
