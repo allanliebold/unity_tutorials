08458962c64324d9ea63e09b00af13e09299afa5
Allan Liebold <allan.liebold@gmail.com>
2018-09-01 12:11:30 -0700

    Update Tile.cs

--- Match3/Scripts/Tutorial/Tile.cs
+++ Match3/Scripts/Tutorial/Tile.cs
@@ -106,5 +106,8 @@ public class Tile : MonoBehaviour {
 		
 		ClearMatch(new Vector2[2] { Vector2.left, Vector2.right });
 		ClearMatch(new Vector2[2] { Vector2.up, Vector2.down });
+		if (matchFound) {
+			
+		}
 	}
 }
