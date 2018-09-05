f3e29c7a0c47722ffd680f2339c818786eb919d1
Allan Liebold <allan.liebold@gmail.com>
2018-09-05 00:23:12 -0700

    Update PlayerSettings.cs

--- BattleGame/PlayerSettings.cs
+++ BattleGame/PlayerSettings.cs
@@ -9,20 +9,21 @@ private Toggle toggle;
 private AudioSource myAudio;
 
 public class PlayerSettings : MonoBehaviour {
-public void Awake () {
-  if (!PlayerPrefs.HasKey("music")) {
-    PlayerPrefs.SetInt("music", 1);
-    toggle.isOn = true;
-    myAudio.enabled = true;
-    PlayerPrefs.Save();
-  } else {
-    if (PlayerPrefs.GetInt("music") == 0) {
-      myAudio.enabled = false;
-      toggle.isOn = false;
-    } else {
-      myAudio.enabled = true;
+  
+  public void Awake () {
+    if (!PlayerPrefs.HasKey("music")) {
+      PlayerPrefs.SetInt("music", 1);
       toggle.isOn = true;
+      myAudio.enabled = true;
+      PlayerPrefs.Save();
+    } else {
+      if (PlayerPrefs.GetInt("music") == 0) {
+        myAudio.enabled = false;
+        toggle.isOn = false;
+      } else {
+        myAudio.enabled = true;
+        toggle.isOn = true;
+      }
     }
   }
-}
 }
