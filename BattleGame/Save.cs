f4150ed40cd13ba3c7a0713cf92257c8cedaea51
Allan Liebold <allan.liebold@gmail.com>
2018-09-06 06:12:32 -0700

    Update Save.cs

--- BattleGame/Save.cs
+++ BattleGame/Save.cs
@@ -5,4 +5,7 @@ using UnityEngine;
 public class Save : MonoBehaviour {
   public List<int> livingTargetPositions = new List<int>();
   public List<int> livingTargetsTypes = new List<int>();
+  
+  public int hits = 0;
+  public int shots = 0;
 }
