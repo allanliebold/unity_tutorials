edb731f1a5ea560c6b353f032ea809f32c9765c6
Allan Liebold <allan.liebold@gmail.com>
2019-06-09 19:38:17 -0700

    Update BalloonPopper.cs

--- Beginner_Tutorial/BalloonPopper.cs
+++ Beginner_Tutorial/BalloonPopper.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BalloonController : MonoBehaviour {
-
+	
+	public float scaleFactor = 1.2f;
+	
 	void Start() {
-
-  }
-
+	
+	}
 
 	void Update() {
 
