8a2a79b61ff4225ecc7c2c9b2af1e88d58874057
Allan Liebold <allan.liebold@gmail.com>
2019-06-09 21:01:54 -0700

    Update BalloonPopper.cs

--- Beginner_Tutorial/BalloonPopper.cs
+++ Beginner_Tutorial/BalloonPopper.cs
@@ -7,11 +7,14 @@ public class BalloonController : MonoBehaviour {
 	public float scaleFactor = 1.2f;
 	public float maxScale = 3f;
 	public float speed = 0.1f;
+	Vector3 vector;
 	
 	void Start() {
 		if(scaleFactor <= 1) {
 			print("scaleFactor too low.");	
 		}
+		
+		vector = new Vector3(1, 1, 1);
 	}
 
 	void Update() {
