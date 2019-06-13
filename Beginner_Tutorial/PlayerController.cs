e178771d5ddcfea072778c3a2a18085aa13893df
Allan Liebold <allan.liebold@gmail.com>
2019-06-13 05:06:36 -0700

    Update PlayerController.cs

--- Beginner_Tutorial/PlayerController.cs
+++ Beginner_Tutorial/PlayerController.cs
@@ -32,6 +32,8 @@ void JumpHandler() {
     if(!pressedJump) {
       Vector3 jumpVector = new Vector3(0, jAxis * jumpForce, 0);
       rb.AddForce(jumpVector, ForceMode.VelocityChange);
+    } else {
+      
     }
   }
 }
