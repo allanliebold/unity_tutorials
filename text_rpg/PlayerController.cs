80148a2cce69c4ab2adb436a2b4efe6211e18817
Allan Liebold <allan.liebold@gmail.com>
2019-06-21 09:40:55 -0700

    Update PlayerController.cs

--- text_rpg/PlayerController.cs
+++ text_rpg/PlayerController.cs
@@ -2,16 +2,4 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-  private int energy;
-  
-  // This is to make energy readable outside of this local space, and changeable inside it. 
-  public int Energy { 
-    get {
-      
-    } 
-    set {
-      
-    }
-  } 
-}
+
