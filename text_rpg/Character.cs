e2019370238bb9c88a9322bbec6b3d261d823341
Allan Liebold <allan.liebold@gmail.com>
2019-06-23 14:06:34 -0700

    Update Character.cs

--- text_rpg/Character.cs
+++ text_rpg/Character.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Character : MonoBehaviour {
-  public int Health { get; set; }
-  public int Strength { get; set; }
-  public int Defense { get; set; }
+  public int HP { get; set; }
+  public int STR { get; set; }
+  public int DEF { get; set; }
+  public int GP { get; set; }
 }
