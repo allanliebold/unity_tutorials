66ed63f01cd9bf62e7a90f9d161db1672260ec75
Allan Liebold <allan.liebold@gmail.com>
2019-06-24 00:03:29 -0700

    Update Character.cs

--- text_rpg/Character.cs
+++ text_rpg/Character.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour {
-  public int HP { get; set; }
-  public int STR { get; set; }
-  public int DEF { get; set; }
-  public int GP { get; set; }
-  public Vector2 RoomIndex { get; set; }
-  public List<string> Inventory { get; set; }
+namespace TextRPG {
+  public class Character : MonoBehaviour {
+    public int HP { get; set; }
+    public int STR { get; set; }
+    public int DEF { get; set; }
+    public int GP { get; set; }
+    public Vector2 RoomIndex { get; set; }
+    public List<string> Inventory { get; set; }
+  }
 }
