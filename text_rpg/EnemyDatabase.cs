cd0c3ca2cc35758c616946d103a5dde6856f8832
Allan Liebold <allan.liebold@gmail.com>
2019-06-27 06:08:20 -0700

    Update EnemyDatabase.cs

--- text_rpg/EnemyDatabase.cs
+++ text_rpg/EnemyDatabase.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 using TextRPG;
 
 public class EnemyDatabase : MonoBehaviour {
-  public List<Enemy> Enemies { get; set; } = new List<Enemy>();  
+  public List<Enemy> Enemies { get; set; } = new List<Enemy>(); 
+  public static EnemyDatabase Instance { get; set; }
 }
