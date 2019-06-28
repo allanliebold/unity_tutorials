fd9258f2f83d80a724b61f0ea6bfd1e19fdf9f9e
Allan Liebold <allan.liebold@gmail.com>
2019-06-28 00:33:07 -0700

    Update Enemy.cs

--- text_rpg/Enemy.cs
+++ text_rpg/Enemy.cs
@@ -2,11 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TextRPG;
 
-namespace TextRPG {
-  public class Enemy : Character {
+public class Enemy : Character {
     public override void TakeDamage(int amount) {
       base.TakeDamage(amount);
     }
-  }
 }
