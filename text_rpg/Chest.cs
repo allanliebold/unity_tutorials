0d3c93962a00c56c7df578f3411e403719165553
Allan Liebold <allan.liebold@gmail.com>
2019-06-30 03:17:40 -0700

    Update Chest.cs

--- text_rpg/Chest.cs
+++ text_rpg/Chest.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 using TextRPG;
 
 public class Chest : MonoBehaviour {
-  
+  public string Item { get; set; }
+  public int Gold { get; set; }
+  public bool Trap { get; set; }
+  public bool Heal { get; set; }
+  public Enemy Enemy { get; set; } 
 }
