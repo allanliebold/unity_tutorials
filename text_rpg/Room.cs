c100154bad23e474a247fd7ab5f45411d0a01c30
Allan Liebold <allan.liebold@gmail.com>
2019-06-29 22:41:41 -0700

    Update Room.cs

--- text_rpg/Room.cs
+++ text_rpg/Room.cs
@@ -14,6 +14,7 @@ public Room () {
   int roll = Random.Range(0, 30);
   if(roll > 0 && roll < 6) {
     Enemy = EnemyDatabase.Instance.GetRandomEnemy();  
+    Enemy.RoomIndex = RoomIndex;
   }
   
 }
