ce4669518525328d319b5b3d8275dc00069faa34
Allan Liebold <allan.liebold@gmail.com>
2019-07-01 05:58:34 -0700

    Update Walrus.cs

--- text_rpg/Walrus.cs
+++ text_rpg/Walrus.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 using TextRPG;
 
 public class Walrus : Enemy {
-  
+  void Start() {
+    Health = 15;
+    Strength = 8;
+    Defense = 8;
+    Gold = 25;
+    Inventory.Add("Tusk");
+  }
 }
