9f865da747efd17725500439dc3cb79e81be3a3d
Allan Liebold <allan.liebold@gmail.com>
2019-07-02 06:12:32 -0700

    Update World.cs

--- text_rpg/World.cs
+++ text_rpg/World.cs
@@ -5,9 +5,13 @@ using TextRPG;
 
 public class World : MonoBehaviour {
   public Room[,] Dungeon { get; set; }  
+  public Vector2 Grid;
   
   public void GenerateFloor() {
-    
+    for(int x = 0; x < Grid.x; x++) {
+      for(int y = 0; y < Grid.y; y++) {
+        
+      }
   }
 }
 
