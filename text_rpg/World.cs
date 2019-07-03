e89768a91c69d4fbf001666b5b6d36f7d4d2a5fe
Allan Liebold <allan.liebold@gmail.com>
2019-07-03 06:15:06 -0700

    Update World.cs

--- text_rpg/World.cs
+++ text_rpg/World.cs
@@ -21,7 +21,7 @@ public class World : MonoBehaviour {
       }
     }
     
-    Vector2 exitLocation = new Vector2();
+    Vector2 exitLocation = new Vector2((int)Random.Range(0, Grid.x), (int)Random.Range(0, Grid.y));
   }
 }
 
