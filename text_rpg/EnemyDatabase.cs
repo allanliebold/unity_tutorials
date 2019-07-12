fed13cc30b7eae412a309f2bf2bcd051ff23fb51
Allan Liebold <allan.liebold@gmail.com>
2019-07-12 06:12:38 -0700

    Update EnemyDatabase.cs

--- text_rpg/EnemyDatabase.cs
+++ text_rpg/EnemyDatabase.cs
@@ -15,7 +15,7 @@ public class EnemyDatabase : MonoBehaviour {
       enemyCount++;
     }
     
-    Debug.Log("Enemy count: " + enemyCount);
+    Debug.Log("Enemies in Database: " + enemyCount);
   }
   
   public Enemy GetRandomEnemy() {
