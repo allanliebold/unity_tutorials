fddf5ae491fbec8048d20fe086c5638d9cbd4967
Allan Liebold <allan.liebold@gmail.com>
2019-07-16 10:46:52 -0700

    Update ScriptableObjects.cs

--- ScriptableObjects.cs
+++ ScriptableObjects.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManager, order = 1)]
 public class SpawnManager : ScriptableObject
 {
+    public string prefabName;
 
+    public int numberOfPrefabsToCreate;
+    public Vector3[] spawnPoints;
 }
