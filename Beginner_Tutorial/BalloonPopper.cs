2c6738a303afd3bee6a5c82ac091c007828c766d
Allan Liebold <allan.liebold@gmail.com>
2019-07-17 06:18:32 -0700

    Update BalloonPopper.cs

--- Beginner_Tutorial/BalloonPopper.cs
+++ Beginner_Tutorial/BalloonPopper.cs
@@ -27,11 +27,11 @@ public class BalloonController : MonoBehaviour {
 	}
 
 	void Update() {
-		transform.position += speed * vector;
-		if (transform.position.y >= maxY && speed > 0) {
+	     transform.position += speed * vector;
+	     if (transform.position.y >= maxY && speed > 0) {
 			speed *= -1;
-		} else if (transform.position.y <= minY && speed < 0) {
+	     } else if (transform.position.y <= minY && speed < 0) {
 			speed *= -1;
-		}
+	     }
 	}
 }
