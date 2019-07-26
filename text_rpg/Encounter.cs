04a742a43f35373a788b92a926951e5d204016d0
Allan Liebold <allan.liebold@gmail.com>
2019-07-26 06:00:52 -0700

    Update Encounter.cs

--- text_rpg/Encounter.cs
+++ text_rpg/Encounter.cs
@@ -30,5 +30,6 @@ public class Encounter : MonoBehaviour {
   public void Attack() {
     int playerDamageAmount = (int)(Random.value * (player.STR - Enemy.DEF));  
     int enemyDamageAmount = (int)(Random.value * (Enemy.STR - player.DEF));
+    player.TakeDamage();
   }
 }
