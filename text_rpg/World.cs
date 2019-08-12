b9c52e81dcabf5f004f5ee36d802ab18c2c92289
Allan Liebold <allan.liebold@gmail.com>
2019-08-11 21:14:20 -0700

    Update World.cs

--- text_rpg/World.cs
+++ text_rpg/World.cs
@@ -20,7 +20,6 @@ public class World : MonoBehaviour {
         };
       }
     }
-    
     Vector2 exitLocation = new Vector2((int)Random.Range(0, Grid.x), (int)Random.Range(0, Grid.y));
     Dungeon[(int)exitLocation.x, (int)exitLocation.y].Exit = true;
     Dungeon[(int)exitLocation.x, (int)exitLocation.y].Empty = false;
