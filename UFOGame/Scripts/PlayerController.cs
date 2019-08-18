7c778378b589bea0f73c30196b7ffbad598b2300
Allan Liebold <allan.liebold@gmail.com>
2019-08-17 23:27:38 -0700

    Update PlayerController.cs

--- UFOGame/Scripts/PlayerController.cs
+++ UFOGame/Scripts/PlayerController.cs
@@ -9,8 +9,7 @@ public class PlayerController : MonoBehaviour {
     rb2d = GetComponent<Rigidbody2D> ();  
   }
   
-  void FixedUpdate () 
-  {
+  void FixedUpdate () {
     float moveHorizontal = Input.GetAxis ("Horizontal");
     float moveVertical = Input.GetAxis ("Vertical");
     Vector2 movement = new Vector2(moveHorizontal, moveVertical);  
